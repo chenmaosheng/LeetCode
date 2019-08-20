2a0615952aea4d4b64c9d3f6008054455dd779d9
chenmaosheng <chanmaosheng@gmail.com>
2019-08-20 20:49:49 +0800

    add 0001 and precomp.h

---
+++ 0001TwoSum.h
@@ -0,0 +1,25 @@
+#ifndef __0001__
+#define __0001__
+
+#include "precomp.h"
+
+vector<int> twoSum(vector<int>& nums, int target) {
+	unordered_map<int, int> m;
+	int size = (int)nums.size();
+	for (int i = 0; i < size; ++i)
+	{
+		if (m.find(target - nums[i]) != m.end())
+			return vector<int>{m[target - nums[i]], i};
+		m[nums[i]] = i;
+	}
+	return vector<int>{-1, -1};
+}
+
+void testcase_0001()
+{
+	vector<int> nums{ 2, 7, 11, 15 };
+	vector<int> res = twoSum(nums, 9);
+	printf("%d %d", res[0], res[1]);
+}
+
+#endif
---
+++ precomp.h
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <vector>
+#include <map>
+#include <queue>
+#include <string>
+#include <numeric> 
+#include <unordered_map>
+#include <unordered_set>
+#include <ctime>
+#include <stack>
+#include <algorithm>
+#include <set>
+#include <bitset>
+#include <memory>
+#include <functional>
+#include <iostream>
+#include <atomic>
+#include <thread>
+#include <new>
+
+using namespace std;
+
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+struct Point {
+	int x;
+	int y;
+	Point() : x(0), y(0) {}
+	Point(int a, int b) : x(a), y(b) {}
+};
